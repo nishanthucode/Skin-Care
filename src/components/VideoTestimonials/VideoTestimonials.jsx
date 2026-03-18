c740f3a5074b82b3536d4cda72ff017e80df3137
Nishanth <nishanth7.mail@gmail.com>
2026-03-18 12:14:29 +0530

    Add new video assets: video-4.mp4 and video-5.mp4

--- src/components/VideoTestimonials/VideoTestimonials.jsx
+++ src/components/VideoTestimonials/VideoTestimonials.jsx
@@ -1,11 +1,137 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FiVolume2, FiVolumeX } from 'react-icons/fi';
+import Hls from 'hls.js';
 import './VideoTestimonials.css';
-import image2 from '../../assets/product-2.jpeg';
 import image1 from '../../assets/product-1.jpeg';
+import image2 from '../../assets/product-2.jpeg';
 import image3 from '../../assets/product-3.jpeg';
 import image4 from '../../assets/product-4.jpeg';
+
+import image21 from '../../assets/image-2.0.png';
+import image31 from '../../assets/image-3.0.png';
+import image41 from '../../assets/image-4.0.png';
+import image51 from '../../assets/image-5.0.png';
+
+import video1 from '../../assets/video-1.mp4';
+import video2 from '../../assets/video-2.mp4';
+import video3 from '../../assets/video-3.mp4';
+import video4 from '../../assets/video-4.mp4';
+import video5 from '../../assets/video-5.mp4';
+
+// HLS Video Player Component
+const HlsVideo = ({ src, autoPlay, muted, loop, playsInline, className }) => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const video = videoRef.current;
+        if (!video || !src) return;
+
+        let hls = null;
+
+        if (src.includes('.m3u8')) {
+            if (Hls.isSupported()) {
+                hls = new Hls({
+                    enableWorker: true,
+                    lowLatencyMode: true,
+                });
+                hls.loadSource(src);
+                hls.attachMedia(video);
+                hls.on(Hls.Events.MANIFEST_PARSED, () => {
+                    if (autoPlay) {
+                        video.play().catch(() => { });
+                    }
+                });
+            } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+                // Safari native HLS support
+                video.src = src;
+                if (autoPlay) {
+                    video.play().catch(() => { });
+                }
+            }
+        } else {
+            // Regular video file
+            video.src = src;
+            if (autoPlay) {
+                video.play().catch(() => { });
+            }
+        }
+
+        return () => {
+            if (hls) {
+                hls.destroy();
+            }
+        };
+    }, [src, autoPlay]);
+
+    return (
+        <video
+            ref={videoRef}
+            className={className}
+            autoPlay={autoPlay}
+            muted={muted}
+            loop={loop}
+            playsInline={playsInline}
+        />
+    );
+};
+
+// Instagram Embed Component
+const InstagramEmbed = ({ url, className }) => {
+    const getEmbedUrl = (instagramUrl) => {
+        // Handle various Instagram URL formats
+        const match = instagramUrl.match(/instagram\.com\/(?:p|reel|reels)\/([A-Za-z0-9_-]+)/);
+        if (match) {
+            return `https://www.instagram.com/p/${match[1]}/embed/`;
+        }
+        // If already an embed URL, return as-is
+        if (instagramUrl.includes('/embed')) {
+            return instagramUrl;
+        }
+        return instagramUrl;
+    };
+
+    return (
+        <iframe
+            className={className}
+            src={getEmbedUrl(url)}
+            frameBorder="0"
+            scrolling="no"
+            allowTransparency="true"
+            allow="autoplay; encrypted-media"
+            allowFullScreen
+            style={{
+                width: '100%',
+                height: '100%',
+                border: 'none',
+                overflow: 'hidden',
+                background: '#000',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+            }}
+        />
+    );
+};
+
+// Universal Video Player - handles HLS and Instagram
+const VideoPlayer = ({ src, autoPlay, muted, loop, playsInline, className }) => {
+    const isInstagram = src && src.includes('instagram.com');
+
+    if (isInstagram) {
+        return <InstagramEmbed url={src} className={className} />;
+    }
+
+    return (
+        <HlsVideo
+            src={src}
+            autoPlay={autoPlay}
+            muted={muted}
+            loop={loop}
+            playsInline={playsInline}
+            className={className}
+        />
+    );
+};
 
 const VideoTestimonials = () => {
     const [hoveredVideo, setHoveredVideo] = useState(null);
@@ -23,45 +149,44 @@ const VideoTestimonials = () => {
     const videos = [
         {
             id: 1,
-            // Using the requested m3u8 URL for hover play
-            webUrl: 'https://youthface.co.in/cdn/shop/videos/c/vp/fe70d588d9144700bcce6b0151c9fe34/fe70d588d9144700bcce6b0151c9fe34.m3u8?v=0',
-            productName: 'Papaya Face Wash (50ml)',
+            webUrl: video1,
+            productName: 'Aura Face Cream',
             price: 199.00,
-            // Using the requested thumbnail
+            productImage: image1,
             thumbnail: 'https://instagram.fixe1-1.fna.fbcdn.net/v/t51.71878-15/568652650_1467514497809228_6741214977905343715_n.jpg?stp=dst-jpg_e15_tt6&_nc_cat=101&ig_cache_key=Mzc0NzQ3MDQyMzg4NzAxNjMwNA%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjY0MHgxMTM2LnNkci5DMyJ9&_nc_ohc=klPIGxRo4aIQ7kNvwGNJRbW&_nc_oc=AdldwpnpcTYpFVFeVnOUI4c0YjEWeiMW-JBgRMX20LOMSwGQIl2ji6ZVhvhhIn75PQtDvJLmaSgiS57zIgLIlhUq&_nc_ad=z-m&_nc_cid=1174&_nc_zt=23&_nc_ht=instagram.fixe1-1.fna&_nc_gid=rrIV-HV1-ppc_X5iK2IPMA&_nc_ss=8&oh=00_AfzByraC5uMgnENFDXHOd2RzGbSGWobez6cO86itznOdow&oe=69BFF7D3',
-            productImage: image3,
         },
+
         {
             id: 2,
-            webUrl: 'https://youthface.co.in/cdn/shop/videos/c/vp/fe70d588d9144700bcce6b0151c9fe34/fe70d588d9144700bcce6b0151c9fe34.m3u8?v=0',
-            productName: 'Papaya Face Wash (50ml)',
+            webUrl: video2,
+            productName: 'Aura Face Cream',
             price: 199.00,
-            thumbnail: 'https://youthface.co.in/cdn/shop/files/preview_images/fe70d588d9144700bcce6b0151c9fe34.thumbnail.0000000000.jpg?v=1752250018&width=720',
-            productImage: image4,
+            thumbnail: image21,
+            productImage: image4,       
         },
         {
             id: 3,
-            webUrl: 'https://youthface.co.in/cdn/shop/videos/c/vp/7a64a636af7f4e328872b68416d58be5/7a64a636af7f4e328872b68416d58be5.m3u8?v=0',
-            productName: 'Beauty Cream (25g)',
-            price: 599.00,
-            thumbnail: 'https://youthface.co.in/cdn/shop/files/preview_images/7a64a636af7f4e328872b68416d58be5.thumbnail.0000000000.jpg?v=1764055048&width=480',
-            productImage: image1,
+            webUrl: video3,
+            productName: 'Aura Face Cream',
+            price: 199.00,
+            thumbnail: image31,
+            productImage: image3,
         },
         {
             id: 4,
-            webUrl: 'https://youthface.co.in/cdn/shop/videos/c/vp/63b3ac2467d94f60b3b8e425da374564/63b3ac2467d94f60b3b8e425da374564.m3u8?v=0',
-            productName: 'Beauty Cream (25g)',
-            price: 599.00,
-            thumbnail: 'https://youthface.co.in/cdn/shop/files/preview_images/63b3ac2467d94f60b3b8e425da374564.thumbnail.0000000000.jpg?v=1752249510&width=600',
-            productImage: image2,
+            webUrl: video4,
+            productName: 'Aura Face Cream',
+            price: 199.00,
+            thumbnail:image41,
+            productImage: image4,
         },
         {
             id: 5,
-            webUrl: '=c76aed8460769.m3u8?v=0',
-            productName: 'Sunscreen (50ml)',
-            price: 399.00,
-            thumbnail: 'https://youthface.co.in/cdn/shop/files/preview_images/f6001e20954544f6b16c76aed8460769.thumbnail.0000000000.jpg?v=1755847917&width=600',
-            productImage: image1,
+            webUrl: video5,
+            productName: 'Aura Face Cream',
+            price: 199.00,
+            thumbnail: image51,
+            productImage: image2,
         },
     ];
 
@@ -94,8 +219,8 @@ const VideoTestimonials = () => {
                         >
                             <div className="v-video-container">
                                 {shouldPlay(video.id, index) ? (
-                                    <video
-                                        src={video.webUrl}
+                                    <VideoPlayer
+                                        src={video.embedUrl || video.webUrl}
                                         className="main-video"
                                         autoPlay
                                         muted
